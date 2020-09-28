1b77446a51f8b5e5207a783ea42238ccdd65e4ea
olliec28 <u1953165@unimail.hud.ac.uk>
2020-09-28 18:25:34 +0100

    Unit 2

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * A demo class.
+ *
+ * @author Oliver Custance
+ * @version September 2020
+ */
+
+public class HelloWorld {
+    /**
+     * A demo method.
+     * @param args command line arguments (not used)
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
---
+++ tests/HelloWorldTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldTest {
+
+    @Test
+    public void passTest(){
+    }
+
+    @Test
+    public void failTest(){
+        fail();
+    }
+
+}
